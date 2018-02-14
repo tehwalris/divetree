465ea3a73de6b9e1d2b9224820cf6411f23c44f3
Philippe Voinov <philippevoinov@gmail.com>
2018-02-15 00:29:33 +0100

    Actually include third party typings.

--- packages/divetree-core/src/index.ts
+++ packages/divetree-core/src/index.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/kiwi.js.d.ts" />
+/// <reference path="../thirdparty/kiwi.js.d.ts" />
 
 export * from "./interfaces/input";
 export { PublicOutputNode as OutputNode } from "./interfaces/output";
