fed6f7a2e2ba4023875b0945e255773818bfaa4b
Philippe Voinov <philippevoinov@gmail.com>
2018-02-14 17:00:17 +0100

    Refactor planAnimation.

--- packages/divetree-core/src/plan-animation.ts
+++ packages/divetree-core/src/plan-animation.ts
@@ -22,6 +22,12 @@ export interface EnterLeaveGroup {
   content: Id[];
 }
 
+interface IdGroup {
+  before: Id[];
+  after: Id[];
+  common: Set<Id>;
+}
+
 function getChildren(node: Node): Node[] {
   switch (node.kind) {
     case NodeKind.Loose:
@@ -31,23 +37,6 @@ function getChildren(node: Node): Node[] {
     case NodeKind.TightLeaf:
       return [];
   }
-}
-
-function treeToIds(
-  root: Node,
-  kind: NodeKind.TightLeaf | NodeKind.Loose,
-): Id[] {
-  const ids: Id[] = [];
-  crawl(
-    root,
-    e => {
-      if (e.kind === kind) {
-        ids.push(e.id);
-      }
-    },
-    { getChildren },
-  );
-  return ids;
 }
 
 function indexTreeNodesById(
@@ -67,6 +56,13 @@ function indexTreeNodesById(
   return out;
 }
 
+function treeToIds(
+  root: Node,
+  kind: NodeKind.TightLeaf | NodeKind.Loose,
+): Id[] {
+  return [...indexTreeNodesById(root, kind).keys()];
+}
+
 function indexTreeParentsByChildren(root: Node): Map<Node, Node> {
   const out = new Map();
   crawl(
@@ -81,59 +77,62 @@ function indexTreeParentsByChildren(root: Node): Map<Node, Node> {
   return out;
 }
 
+function loadIdGroup(
+  beforeRoot: Node,
+  afterRoot: Node,
+  kind: NodeKind.TightLeaf | NodeKind.Loose,
+): IdGroup {
+  const before = treeToIds(beforeRoot, kind);
+  const after = treeToIds(afterRoot, kind);
+  return {
+    before,
+    after,
+    common: new Set(R.intersection(before, after)),
+  };
+}
+
 export function planAnimation(before: Node, after: Node): AnimationGroup[] {
   const animationGroups: AnimationGroup[] = [];
-
-  const tightIds = {
-    before: treeToIds(before, NodeKind.TightLeaf),
-    after: treeToIds(after, NodeKind.TightLeaf),
-    common: [] as Id[],
+  const ids = {
+    tight: loadIdGroup(before, after, NodeKind.TightLeaf),
+    loose: loadIdGroup(before, after, NodeKind.Loose),
   };
-  tightIds.common = R.intersection(tightIds.before, tightIds.after);
 
-  if (tightIds.common.length) {
+  if (ids.tight.common.size) {
     animationGroups.push({
       kind: AnimationKind.Transform,
-      content: tightIds.common,
+      content: [...ids.tight.common],
     });
   }
-
-  const looseIds = {
-    before: treeToIds(before, NodeKind.Loose),
-    after: treeToIds(after, NodeKind.Loose),
-    common: new Set<Id>(),
-  };
-  looseIds.common = new Set(R.intersection(looseIds.before, looseIds.after));
 
   [
     {
       kind: AnimationKind.Leave as AnimationKind.Leave,
       tree: before,
-      tightIdsOwn: tightIds.before,
+      tightIdsOwn: ids.tight.before,
     },
     {
       kind: AnimationKind.Enter as AnimationKind.Enter,
       tree: after,
-      tightIdsOwn: tightIds.after,
+      tightIdsOwn: ids.tight.after,
     },
   ].forEach(({ tree, kind, tightIdsOwn }) => {
     const parents = indexTreeParentsByChildren(tree);
     const tightById = indexTreeNodesById(tree, NodeKind.TightLeaf);
     const changingByParent = new Map<Id | undefined, Id[]>();
 
-    R.difference(tightIdsOwn, tightIds.common).forEach(id => {
-      let changeParent: Node | undefined;
+    R.difference(tightIdsOwn, [...ids.tight.common]).forEach(id => {
       let cur = tightById.get(id);
       while (
         cur &&
-        !(cur.kind === NodeKind.Loose && looseIds.common.has(cur.id))
+        !(cur.kind === NodeKind.Loose && ids.loose.common.has(cur.id))
       ) {
         cur = parents.get(cur);
       }
-      const parent = cur && cur.id;
-      const group = changingByParent.get(parent) || [];
+      const changeParent = cur && cur.id;
+      const group = changingByParent.get(changeParent) || [];
       group.push(id);
-      changingByParent.set(parent, group);
+      changingByParent.set(changeParent, group);
     });
 
     changingByParent.forEach((content, parent) => {
@@ -147,43 +146,3 @@ export function planAnimation(before: Node, after: Node): AnimationGroup[] {
 
   return animationGroups;
 }
-
-/*
-- iterate before tree
-  - everything which isn't common is leaving
-  - parent for leaving is closest common (=not leaving) loose node
-- iterate after tree
-  - everything which isn't common is entering
-  - parent for entering is closest common (=not leaving) loose node
-*/
-
-/*
-- what to do with tight parent node?
-  - what if it's a split? handle enter/leave the usual way?
-    - actually that's pretty good
-- what is entering/leaving?
-  - entering/leaving without looking at groups:
-    - anything not in commonIds
-  - only leaf nodes have ids!
-    - but there is a leaf in every loose node, so loose nodes have ids
-      - except loose nodes with split parent!
-    - give synthetic ids to loose nodes?
-      - really complicated for no good reason
-    - require ids on loose nodes
-      - consequences?
-        - nodes inside tight parents will always leave/enter relative to tight bounding box
-*/
-
-/*
-  - final
-    - mark transformed
-      - collect all ids in before tree
-      - collect all ids in after tree
-      - create set with intersection
-      - create transform group from set
-    - enter/leave
-      - track closest loose id (as parent)
-      - iterate down tree
-      - when cur is entering/leaving, take whole subtree (with cur)
-      - parent is parent of subtree
-*/
