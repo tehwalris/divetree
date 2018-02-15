10c1b0245951aae79351f166efea2fb4a2767b5e
Philippe Voinov <philippevoinov@gmail.com>
2018-02-15 17:24:27 +0100

    Slider controlled animation.

--- divetree-demo/src/App.tsx
+++ divetree-demo/src/App.tsx
@@ -1,161 +1,9 @@
 import * as React from "react";
-import { doLayoutAnimated, NodeKind, Split, LooseNode } from "divetree-core";
+import { doLayoutAnimated } from "divetree-core";
+import { beforeTree, afterTree } from "./data";
+import Slider from "rc-slider";
 
-const beforeTree: LooseNode = {
-  kind: NodeKind.Loose,
-  id: "root",
-  parent: {
-    kind: NodeKind.TightLeaf,
-    id: "for i in 1...100",
-    size: [150, 50],
-  },
-  children: [
-    {
-      kind: NodeKind.Loose,
-      id: "if-loose",
-      parent: {
-        kind: NodeKind.TightLeaf,
-        id: "if",
-        size: [50, 50],
-      },
-      children: [
-        {
-          kind: NodeKind.Loose,
-          id: "fizzbuzz-loose",
-          parent: {
-            kind: NodeKind.TightSplit,
-            split: Split.SideBySide,
-            children: [
-              {
-                kind: NodeKind.TightLeaf,
-                id: "and",
-                size: [50, 50],
-              },
-              {
-                kind: NodeKind.TightSplit,
-                split: Split.Stacked,
-                children: [
-                  {
-                    kind: NodeKind.TightLeaf,
-                    id: "and-part-1",
-                    size: [110, 25],
-                  },
-                  {
-                    kind: NodeKind.TightLeaf,
-                    id: "and-part-2",
-                    size: [110, 25],
-                  },
-                ],
-              },
-            ],
-          },
-          children: [
-            {
-              kind: NodeKind.TightLeaf,
-              id: "print-fizzbuzz",
-              size: [180, 50],
-            },
-          ],
-        },
-        ...[0, 1, 2].map((i: number): LooseNode => ({
-          kind: NodeKind.Loose,
-          id: `${i}-loose`,
-          parent: {
-            kind: NodeKind.TightLeaf,
-            id: `condition-${i}`,
-            size: [160, 50],
-          },
-          children: [
-            {
-              kind: NodeKind.TightLeaf,
-              id: `print-${i}`,
-              size: [150, 50],
-            },
-          ],
-        })),
-      ],
-    },
-  ],
-};
-
-const afterTree: LooseNode = {
-  kind: NodeKind.Loose,
-  id: "root",
-  parent: {
-    kind: NodeKind.TightLeaf,
-    id: "for i in 1...100",
-    size: [150, 50],
-  },
-  children: [
-    {
-      kind: NodeKind.Loose,
-      id: "if-loose",
-      parent: {
-        kind: NodeKind.TightLeaf,
-        id: "if",
-        size: [50, 50],
-      },
-      children: [
-        {
-          kind: NodeKind.Loose,
-          id: "fizzbuzz-loose",
-          parent: {
-            kind: NodeKind.TightSplit,
-            split: Split.SideBySide,
-            children: [
-              {
-                kind: NodeKind.TightLeaf,
-                id: "and",
-                size: [50, 50],
-              },
-              {
-                kind: NodeKind.TightSplit,
-                split: Split.Stacked,
-                children: [
-                  {
-                    kind: NodeKind.TightLeaf,
-                    id: "and-part-1",
-                    size: [110, 25],
-                  },
-                  {
-                    kind: NodeKind.TightLeaf,
-                    id: "and-part-2",
-                    size: [110, 25],
-                  },
-                ],
-              },
-            ],
-          },
-          children: [
-            {
-              kind: NodeKind.TightLeaf,
-              id: "print-fizzbuzz",
-              size: [180, 50],
-            },
-          ],
-        },
-        ...[0, 2, 3].map((i: number): LooseNode => ({
-          kind: NodeKind.Loose,
-          id: `${i}-loose`,
-          parent: {
-            kind: NodeKind.TightLeaf,
-            id: `condition-${i}`,
-            size: [160, 50],
-          },
-          children: [
-            {
-              kind: NodeKind.TightLeaf,
-              id: `print-${i}`,
-              size: [150, 50],
-            },
-          ],
-        })),
-      ],
-    },
-  ],
-};
-
-const layoutConfig = {
+const LAYOUT_CONFIG = {
   loose: {
     verticalPadding: 0,
     siblingDistance: 20,
@@ -167,10 +15,10 @@ const layoutConfig = {
   },
 };
 
-const output = doLayoutAnimated(beforeTree, afterTree, layoutConfig);
+const output = doLayoutAnimated(beforeTree, afterTree, LAYOUT_CONFIG);
 
 const styles: { [key: string]: React.CSSProperties } = {
-  wrapper: {
+  treeWrapper: {
     position: "relative",
   },
   rect: {
@@ -181,21 +29,40 @@ const styles: { [key: string]: React.CSSProperties } = {
 };
 
 class App extends React.Component {
+  state: {
+    t: number;
+  } = { t: 0 };
+
+  onSliderChange = (t: number) => {
+    this.setState({ t });
+  };
+
   render() {
+    const { t } = this.state;
     return (
-      <div style={styles.wrapper}>
-        {output(0.75).map(e => (
-          <div
-            key={e.id}
-            style={{
-              ...styles.rect,
-              top: e.offset[1],
-              left: e.offset[0],
-              width: e.size[0],
-              height: e.size[1],
-            }}
-          />
-        ))}
+      <div>
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          onChange={this.onSliderChange}
+          value={t}
+        />
+        <div style={styles.treeWrapper}>
+          {output(t).map(e => (
+            <div
+              key={e.id}
+              style={{
+                ...styles.rect,
+                top: e.offset[1],
+                left: e.offset[0],
+                width: e.size[0],
+                height: e.size[1],
+                opacity: 1 - Math.abs(e.lifecycle),
+              }}
+            />
+          ))}
+        </div>
       </div>
     );
   }
---
+++ divetree-demo/src/data.ts
@@ -0,0 +1,155 @@
+import { NodeKind, Split, LooseNode } from "divetree-core";
+
+export const beforeTree: LooseNode = {
+  kind: NodeKind.Loose,
+  id: "root",
+  parent: {
+    kind: NodeKind.TightLeaf,
+    id: "for i in 1...100",
+    size: [150, 50],
+  },
+  children: [
+    {
+      kind: NodeKind.Loose,
+      id: "if-loose",
+      parent: {
+        kind: NodeKind.TightLeaf,
+        id: "if",
+        size: [50, 50],
+      },
+      children: [
+        {
+          kind: NodeKind.Loose,
+          id: "fizzbuzz-loose",
+          parent: {
+            kind: NodeKind.TightSplit,
+            split: Split.SideBySide,
+            children: [
+              {
+                kind: NodeKind.TightLeaf,
+                id: "and",
+                size: [50, 50],
+              },
+              {
+                kind: NodeKind.TightSplit,
+                split: Split.Stacked,
+                children: [
+                  {
+                    kind: NodeKind.TightLeaf,
+                    id: "and-part-1",
+                    size: [110, 25],
+                  },
+                  {
+                    kind: NodeKind.TightLeaf,
+                    id: "and-part-2",
+                    size: [110, 25],
+                  },
+                ],
+              },
+            ],
+          },
+          children: [
+            {
+              kind: NodeKind.TightLeaf,
+              id: "print-fizzbuzz",
+              size: [180, 50],
+            },
+          ],
+        },
+        ...[0, 1, 2].map((i: number): LooseNode => ({
+          kind: NodeKind.Loose,
+          id: `${i}-loose`,
+          parent: {
+            kind: NodeKind.TightLeaf,
+            id: `condition-${i}`,
+            size: [160, 50],
+          },
+          children: [
+            {
+              kind: NodeKind.TightLeaf,
+              id: `print-${i}`,
+              size: [150, 50],
+            },
+          ],
+        })),
+      ],
+    },
+  ],
+};
+
+export const afterTree: LooseNode = {
+  kind: NodeKind.Loose,
+  id: "root",
+  parent: {
+    kind: NodeKind.TightLeaf,
+    id: "for i in 1...100",
+    size: [150, 50],
+  },
+  children: [
+    {
+      kind: NodeKind.Loose,
+      id: "if-loose",
+      parent: {
+        kind: NodeKind.TightLeaf,
+        id: "if",
+        size: [50, 50],
+      },
+      children: [
+        {
+          kind: NodeKind.Loose,
+          id: "fizzbuzz-loose",
+          parent: {
+            kind: NodeKind.TightSplit,
+            split: Split.SideBySide,
+            children: [
+              {
+                kind: NodeKind.TightLeaf,
+                id: "and",
+                size: [50, 50],
+              },
+              {
+                kind: NodeKind.TightSplit,
+                split: Split.Stacked,
+                children: [
+                  {
+                    kind: NodeKind.TightLeaf,
+                    id: "and-part-1",
+                    size: [110, 25],
+                  },
+                ],
+              },
+            ],
+          },
+          children: [
+            {
+              kind: NodeKind.TightLeaf,
+              id: "print-fizzbuzz",
+              size: [180, 50],
+            },
+            {
+              kind: NodeKind.TightLeaf,
+              id: "and-part-2",
+              size: [180, 50],
+            },
+          ],
+        },
+        ...[0, 2, 3].map((i: number): LooseNode => ({
+          kind: NodeKind.Loose,
+          id: `${i}-loose`,
+          parent: {
+            kind: NodeKind.TightLeaf,
+            id: `condition-${i}`,
+            size: [160, 50],
+          },
+          children: [
+            {
+              kind: NodeKind.TightLeaf,
+              id: `print-${i}`,
+              size: [150, 50],
+            },
+          ],
+        })),
+      ],
+    },
+  ],
+};
