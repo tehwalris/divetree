ce4fcdf121bd22cf7d49699a61ff0a93110ceab0
Philippe Voinov <philippevoinov@gmail.com>
2021-07-20 13:29:01 +0200

    Allow setting border color on rects

--- divetree-react/src/rects.tsx
+++ divetree-react/src/rects.tsx
@@ -6,6 +6,7 @@ export type GetContent = (id: Id) => React.ReactElement<unknown> | null;
 
 export interface RectStyle {
   color: number[];
+  borderColor: number[];
 }
 
 export type GetStyle = (id: Id, focused: boolean) => RectStyle;
@@ -21,7 +22,9 @@ interface Props {
 }
 
 const DEFAULT_GET_STYLE: GetStyle = (id, focused) =>
-  focused ? { color: [0, 128, 0] } : { color: [245, 222, 179] };
+  focused
+    ? { color: [0, 128, 0], borderColor: [0, 0, 0] }
+    : { color: [245, 222, 179], borderColor: [0, 0, 0] };
 
 const styles: { [key: string]: React.CSSProperties } = {
   treeWrapper: {
@@ -46,14 +49,34 @@ function padColor(color: number[]): number[] {
   }
 }
 
-function getFocusColor(progress: number, id: Id, getStyle: GetStyle): string {
-  const a = padColor(getStyle(id, false).color);
-  const b = padColor(getStyle(id, true).color);
+function mixColors(progress: number, _a: number[], _b: number[]): string {
+  const a = padColor(_a);
+  const b = padColor(_b);
   const mixed = a.map((e, i) => e * (1 - progress) + b[i] * progress);
   for (let i = 0; i < 3; i++) {
     mixed[i] = Math.floor(mixed[i]);
   }
   return `rgba(${mixed.join(", ")})`;
+}
+
+function getFocusColor(progress: number, id: Id, getStyle: GetStyle): string {
+  return mixColors(
+    progress,
+    getStyle(id, false).color,
+    getStyle(id, true).color,
+  );
+}
+
+function getFocusBorderColor(
+  progress: number,
+  id: Id,
+  getStyle: GetStyle,
+): string {
+  return mixColors(
+    progress,
+    getStyle(id, false).borderColor,
+    getStyle(id, true).borderColor,
+  );
 }
 
 const DPR = window.devicePixelRatio;
@@ -101,6 +124,11 @@ export const Rects = ({
               opacity: 1 - Math.abs(e.lifecycle),
               zIndex: 1 - Math.ceil(Math.abs(e.lifecycle)),
               background: getFocusColor(
+                focus ? Math.abs(focus.progress) : 0,
+                e.id,
+                getStyle || DEFAULT_GET_STYLE,
+              ),
+              borderColor: getFocusBorderColor(
                 focus ? Math.abs(focus.progress) : 0,
                 e.id,
                 getStyle || DEFAULT_GET_STYLE,
